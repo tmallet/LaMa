68bfee5e81ad5f619dd2abe5294645529cae0da4
Thimal <thibaut3mallet@gmail.com>
2019-04-04 00:18:25 +0200

    Resizing par step

--- src/components/Workspace/WorkspaceCol.vue
+++ src/components/Workspace/WorkspaceCol.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="lm-col" @mousedown="mousedown">{{isResizing}}</div>
+  <div class="lm-col" @mousedown.left="mousedown" v-bind:style="{width: width+'px'}"></div>
 </template>
 
 <script>
@@ -7,23 +7,50 @@ export default {
   name: "WorkspaceCol",
   data() {
     return {
-      isResizing: null
+      isResizing: null,
+      initialX: null,
+      width: 65,
+      prevWidth: 65
     };
   },
   mounted() {
-    document.addEventListener("mouseup", () => {
-      this.isResizing = false;
-    });
-    document.addEventListener("mousemove", e => {
-      if (this.isResizing) {
-        console.log(e);
-      }
-    });
+    document.addEventListener("mouseup", this.mouseup);
+    document.addEventListener("mousemove", this.mousemove);
+  },
+  destroyed() {
+    document.removeEventListener("mouseup", this.mouseup);
+    document.removeEventListener("mousemove", this.mousemove);
   },
   methods: {
     mousedown(e) {
       e.preventDefault();
       this.isResizing = true;
+      this.initialX = e.x;
+    },
+    mouseup() {
+      this.isResizing = false;
+      this.initialX = null;
+      if (this.width > 0 && this.width < 107.5) {
+        this.width = 65;
+      } else if (this.width > 890) {
+        this.width = 890;
+      } else {
+        for (let i = 0; i < 12; i++) {
+          if (this.width >= 107.5 + i * 75 && this.width < 182.5 + i * 75) {
+            this.width = 140 + i * 75;
+            break;
+          }
+        }
+      }
+      this.prevWidth = this.width;
+    },
+    mousemove(e) {
+      if (this.isResizing) {
+        const newWidth = this.prevWidth + e.x - this.initialX;
+        if (newWidth >= 65) {
+          this.width = this.prevWidth + e.x - this.initialX;
+        }
+      }
     }
   }
 };
@@ -34,7 +61,6 @@ export default {
   height: 150px;
   margin-left: 10px;
   margin-bottom: 15px;
-  width: 65px;
 
   background-color: blueviolet;
 
